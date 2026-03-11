4dc3dd7171c8d44cba98ffe21838c51c4cc87b65
summon10 <goodfella9091@gmail.com>
2026-03-11 17:10:57 +0300

    added environment variables, config for tomcat, hikari pool connection, liquibase

---
+++ src/main/resources/db/changelog/changeset-002-insert-data.sql
@@ -0,0 +1,5 @@
+-- liquibase formatted sql
+
+-- changeset summon10:2
+INSERT INTO wallets (id, balance) VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', '10000.00');
+INSERT INTO wallets (id, balance) VALUES ('b1ffcd00-0d1c-4fa9-cc7e-7cc0ce491b22', '50.00');
